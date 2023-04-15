86ea487fdc1a4fa188e159f035db484bff567704
Yuying Fan <yuyingfan@Yuyings-MacBook-Air.local>
2023-04-15 15:59:38 -0400

    set up backend

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const config = require('./config');
+const routes = require('./routes');
+
+const app = express();
+app.use(cors({
+  origin: '*',
+}));
+
+// We use express to define our various API endpoints and
+// provide their handlers that we implemented in routes.js
+app.get('/', routes.parks)
+app.get('/parks', routes.parks);
+app.get('/random', routes.random);
+
+app.listen(config.server_port, () => {
+  console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
+});
+
+module.exports = app;
